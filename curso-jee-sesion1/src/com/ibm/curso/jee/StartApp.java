86c699ddde36b200f8523d388260a2db52f38318
LUIS BASTIAN GUTIERREZ GUTIERREZ <luis.bastian.gutierrez@ibm.com>
2019-07-22 14:07:39 -0400

    actualizamos version del mensaje a desplegar por pantalla

--- curso-jee-sesion1/src/com/ibm/curso/jee/StartApp.java
+++ curso-jee-sesion1/src/com/ibm/curso/jee/StartApp.java
@@ -3,8 +3,8 @@ package com.ibm.curso.jee;
 public class StartApp {
 
 	public static void main(String[] args) {
-		System.out.println("Version 1.2");
-		System.out.println("Hello World!");
+		System.out.println("Version 1.3");
+		System.out.println("Hello World?");
 	}
 
 }
